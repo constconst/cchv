f66814ed167680b90ceb74905b8bc8da96c6217d
Constanza Barrios <co.fuenzalida@gmail.com>
2021-04-23 23:46:43 -0400

    añadi js, saqué el masonry y puse fixed el header

--- app.js
+++ app.js
@@ -1,4 +1,32 @@
+// Tabs que muestran los contenidos de los programas (en index y en page)
+
+
 $('#list-tab a').on('mouseenter', function(e) {
     e.preventDefault()
     $(this).tab('show')
 })
+
+
+// cambiar color del nav después de hacer scroll
+
+$(function() {
+    $(document).scroll(function() {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $(window).height());
+        var $navlink = $(".navbar a");
+        $navlink.toggleClass('scrolled', $(this).scrollTop() > $(window).height());
+    });
+});
+
+
+var myNav = document.getElementById('navbar-color');
+window.onscroll = function() {
+    "use strict";
+    if (document.body.scrollTop >= 200) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
